eaec1b6b65527805eeb238e10a142c644f8a834b
GilbertoSilva998 <Gil.moreno998@gmail.com>
2023-10-20 09:35:19 +0200

    Add User & Driver Page added

--- src/drivers/AddDriver.js
+++ src/drivers/AddDriver.js
@@ -1,48 +1,25 @@
-import React, {useState} from 'react'
+import React from "react";
 import axios from "axios";
-import {Link, useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
 
-export default function AddUser() {
-
-    let navigate=useNavigate()
-
-    const [user,setUser] =useState({
-       userName:"",
-       userAddress:"",
-       userEmail: "",
-       userPhone:""
-
-   })
-
-    const{userName, userAddress, userEmail, userPhone}=user
-
-    const onInputChange= (e)=> {
-        setUser(...user, {[e.target.userName]:e.target.value})
-    }
-
-    const onSubmit =async (e) =>{
-        e.preventDefault();
-        await axios.post("http://localhost:8080/user", user)
-        navigate("/")
-    };
-
+export default function AddDriver() {
     return (
         <div className="container">
             <div className="row">
                 <div className="col md-6 offset-md-3 border rounded p-4 mt-2 shadow">
                     <h2 className="text-center m-4">Register User</h2>
-                   {/*Name*/}
+                    {/*Name*/}
                     <div className="mb-3">
                         <label htmlFor="Name" className="form-label">
                             Name
                         </label>
                         <input
-                        type={"text"}
-                        className="form-control"
-                        placeholder="Enter your name"
-                        name={userName}
-                        value={userName}
-                        onChange={(e)=>onInputChange(e)}
+                            type={"text"}
+                            className="form-control"
+                            placeholder="Enter your name"
+                            name={userName}
+                            value={userName}
+                            onChange={(e)=>onInputChange(e)}
                         />
                     </div>
                     {/*Address*/}
@@ -89,9 +66,9 @@ export default function AddUser() {
                     </div>
                     {/*Button*/}
                     <button type="submit" className="btn btn-outline-primary">
-                    Submit
+                        Submit
                     </button>
-                    <Link className="btn btn-outline-danger mx-2" to="/">
+                    <Link type="submit" className="btn btn-outline-danger mx-2" to="/">
                         Cancel
                     </Link>
                 </div>
